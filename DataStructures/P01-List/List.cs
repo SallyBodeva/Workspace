d9e444bb20f499da5705c502f0b8decf00258211
salihe <salihe.bodeva10@gmail.com>
2024-09-11 23:14:10 +0300

    ...

--- DataStructures/P01-List/List.cs
+++ DataStructures/P01-List/List.cs
@@ -7,37 +7,5 @@ using System.Threading.Tasks;
 
 public class List<T>
 {
-    public T[] basic = new T[0];
-
-    public void Add(T element)
-    {
-        T[] oldArrayValues = basic;
-        basic = new T[basic.Length + 1];
-        Array.Copy(oldArrayValues, basic, oldArrayValues.Length);
-        basic[basic.Length - 1] = element;
-    }
-
-    public int Count()
-    {
-        return basic.Length;
-    }
-
-    public void Insert(int index, T element)
-    {
-        if (index >= 0 && index < basic.Length)
-        {
-            T[] oldArrayValues = basic;
-            basic = new T[basic.Length + 1];
-            //TODO
-        }
-    }
-    public T Get(int index)
-    {
-        if (index >= 0 && index < basic.Length)
-        {
-            return basic[index];
-        }
-        throw new IndexOutOfRangeException();
-    }
-
+    
 }
